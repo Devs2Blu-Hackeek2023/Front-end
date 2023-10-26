7a135a9af54c84d03235ee6f3a0d40a410f535b3
LucasDS0608 <dickmannschneiderlucas@gmail.com>
2023-10-25 23:31:53 -0300

    Implementação do gráfico da tela home

---
+++ co2now/lib/bar_graph/individual_bar.dart
@@ -0,0 +1,6 @@
+class IndividualBar {
+  final int x; //Posição
+  final double y; //Tamanho
+
+  IndividualBar({required this.x, required this.y});
+}
--- co2now/lib/screens/home_screen.dart
+++ co2now/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:co2now/bar_graph/bar_graph_regions/bar_graph.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +10,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<double> regionSummary = [
+    4.40,
+    42.42,
+    10.50,
+    88.99,
+    90.10
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children:[ 
+            Container(
+            height: 415,
+            width: 625,
+            child: BarGraph(
+              regionSummary: regionSummary,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                child: Column(
+                  children: [
+                    Icon(Icons.car_rental, size: 60,),
+                    Text("Teste 1")
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Icon(Icons.car_rental, size: 60,),
+                    Text("Teste 1")
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Icon(Icons.car_rental, size: 60,),
+                    Text("Teste 1")
+                  ],
+                ),
+              ),
+            ],
+          )
+          ]
+        ),
+      )
     );
   }
 }
