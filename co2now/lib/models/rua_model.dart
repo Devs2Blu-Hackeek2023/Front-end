c541477820f68dda26514d5df8ce0964121039fc
Gabriel Dickmann Schneider <dickmannschneidergabriel@gmail.com>
2023-10-28 18:45:23 -0300

    Adicionando os repositórios e os modelos

---
+++ co2now/lib/models/rua_model.dart
@@ -0,0 +1,25 @@
+class RuaModel {
+  late int id;
+  late String cep;
+  late int comprimento;
+
+  RuaModel({
+    required this.id,
+    required this.cep,
+    required this.comprimento,
+  });
+
+  RuaModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    cep = json['cep'];
+    comprimento = json['comprimento'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'cep': cep,
+      'comprimento': comprimento,
+    };
+  }
+}
---
+++ co2now/lib/repositories/usuario_repository.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:co2now/models/usuario_model.dart';
+import 'package:http/http.dart' as http;
+
+class UsuarioRepository {
+  static Future<void> createUsuario(UsuarioModel usuario) async {
+    try {
+      Uri uri = Uri.parse("https://api.co2now.devs2blu.dev.br/api/Usuario");
+      final response = await http.post(
+        uri,
+        body: jsonEncode(usuario.toJson()),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 201) {
+        // Usuário criado com sucesso
+      } else {
+        throw("Erro ao criar um novo usuário. Status Code: ${response.statusCode}");
+      }
+    } catch (e) {
+      throw("Erro ao fazer a solicitação HTTP: $e");
+    }
+  }
+
+  static Future<UsuarioModel> getUsuarioById(int id) async {
+    try {
+      Uri uri = Uri.parse("https://api.co2now.devs2blu.dev.br/api/Usuario/$id");
+      final response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        UsuarioModel usuario = UsuarioModel.fromJson(data);
+        return usuario;
+      } else {
+        throw("Erro ao obter o usuário. Status Code: ${response.statusCode}");
+      }
+    } catch (e) {
+      throw("Erro ao fazer a solicitação HTTP: $e");
+    }
+  }
+}
